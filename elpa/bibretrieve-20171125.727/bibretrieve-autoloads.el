df46a6de464823055b23325cd58d2ab46da4354a
Liam Packer <lpacker1@swarthmore.edu>
2018-03-25 18:31:49 -0400

    cleanup

--- elpa/bibretrieve-20171125.727/bibretrieve-autoloads.el
+++
@@ -1,36 +0,0 @@
-;;; bibretrieve-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "bibretrieve-base" "bibretrieve-base.el" (23152
-;;;;;;  53478 52840 695000))
-;;; Generated autoloads from bibretrieve-base.el
-
-(autoload 'bibretrieve "bibretrieve-base" "\
-Search the web for bibliography entries.
-
-After prompting for author and title, searches on the web, using the
-backends specified by the customization variable
-`bibretrieve-backends'.  A selection process (using RefTeX Selection)
-allows to select entries to add to the current buffer or to a
-bibliography file.
-
- When called with a `C-u' prefix, permits to select the backend and the
- timeout for the search.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("bibretrieve-pkg.el" "bibretrieve.el")
-;;;;;;  (23152 53478 78440 837000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; bibretrieve-autoloads.el ends here
--- elpa/bibretrieve-20171125.727/bibretrieve-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "bibretrieve" "20171125.727" "Retrieve BibTeX entries from the internet"
-  '((auctex "11.87")
-    (emacs "24.3"))
-  :url "https://github.com/pzorin/bibretrieve" :keywords
-  '("bibtex" "bibliography" "mathscinet" "arxiv" "zbmath"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
