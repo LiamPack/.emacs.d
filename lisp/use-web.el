8b04ced76fb15b43dbfdcb9c57f27cd17fdb3500
Liam Packer <liampacker@gmail.com>
2018-05-27 23:42:26 -0400

    added erc! stuff. also don't forget to change some use-web.el stuff back to uncommented..

--- lisp/use-web.el
+++ lisp/use-web.el
@@ -50,22 +50,6 @@
   (add-hook 'emmet-mode-hook (lambda () (setq emmet-indent-after-insert nil)))
   (setq emmet-move-cursor-between-quotes t))
 
-<<<<<<< HEAD
-=======
-;; more info here https://github.com/skeeto/skewer-mode
-;; check it out https://github.com/smihica/emmet-mode
-;; C-x C-e: Evaluate the form before the point and display the result in the minibuffer. If given a prefix argument, insert the result into the current buffer.
-;; C-M-x: Evaluate the top-level form around the point.
-;; C-c C-k: Load the current buffer.
-;; C-c C-z: Select the REPL buffer.
-(use-package skewer-mode ; interactive repl for javascript / web dev
-  :ensure t
-  :config
-  (add-hook 'js2-mode-hook 'skewer-mode)
-  (add-hook 'css-mode-hook 'skewer-css-mode)
-  (add-hook 'html-mode-hook 'skewer-html-mode))
->>>>>>> 161bc3434b2e767f8993a39d31d76bd4771379c6
-
 (use-package impatient-mode ; auto-update browser without having to
                             ; reload when editing web stuff
   :defer t
@@ -121,7 +105,7 @@
                     "setTimeout" "setInterval" "location" "skewer"
                     "console" "phantom"))))
 
-<<<<<<< HEAD
+
 ;; more info here https://github.com/skeeto/skewer-mode
 ;; check it out https://github.com/smihica/emmet-mode
 ;; C-x C-e: Evaluate the form before the point and display the result in the minibuffer. If given a prefix argument, insert the result into the current buffer.
