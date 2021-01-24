a9ca21d1b6ccb5671f8f6f0779848be662a3e540
Liam <liampacker@gmail.com>
2021-01-24 16:43:29 -0500

    lsp and c-env updates from a more informed place

--- lisp/lp-c-env.el
+++ lisp/lp-c-env.el
@@ -17,8 +17,51 @@
   :hook
   (c-common-mode-hook . hs-minor-mode)
   :config
-  (define-key c++-mode-map (kbd "C-c C-c") 'compile)
-  (define-key c++-mode-map (kbd "C-c C-k") 'kill-compilation))
+  (setq compilation-ask-about-save nil)
+  (setq compilation-scroll-output 'next-error)
+
+  (setq compilation-skip-threshold 2)
+  (defcustom endless/compile-window-size 55
+    "Width given to the non-compilation window."
+    :type 'integer
+    :group 'endless)
+
+  (defun endless/compile-please (comint)
+    "Compile without confirmation.
+With a prefix argument, use comint-mode."
+    (interactive "P")
+    ;; Do the command without a prompt.
+    (save-window-excursion
+      (compile (eval compile-command) (and comint t)))
+    ;; Create a compile window of the desired width.
+    (pop-to-buffer (get-buffer "*compilation*"))
+    (enlarge-window
+     (- (frame-width)
+        endless/compile-window-size
+        (window-width))
+     'horizontal))
+
+  (setq tab-width 4)
+  (setq c-basic-offset 4)
+  (setq-default indent-tabs-mode nil)
+  (add-hook 'c-mode-hook #'skeeto/c-hook)
+  (add-to-list 'c-default-style '(c-mode . "k&r"))
+  (add-to-list 'c-default-style '(c++-mode . "k&r"))
+  (add-hook 'c-common-mode-hook (lambda ()
+                                  (hs-minor-mode t)
+                                  (local-set-key (kbd "C-c C-]") 'hs-show-block)
+                                  (local-set-key (kbd "C-c C-_")  'hs-hide-block)
+                                  (local-set-key (kbd "C-M-_")    'hs-hide-all)
+                                  (local-set-key (kbd "C-M-]")  'hs-show-all)))
+  (add-hook 'c++-mode-hook (lambda ()
+                             (hs-minor-mode t)
+                             (local-set-key (kbd "C-c C-]") 'hs-show-block)
+                             (local-set-key (kbd "C-c C-_")  'hs-hide-block)
+                             (local-set-key (kbd "C-M-_")    'hs-hide-all)
+                             (local-set-key (kbd "C-M-]")  'hs-show-all)))
+  (define-key c++-mode-map (kbd "C-c C-c") 'endless/compile-please)
+  (define-key c++-mode-map (kbd "C-c C-k") 'kill-compilation)
+  (define-key c++-mode-map (kbd "C-c u") 'clang-format-buffer))
 
 ;; Load CUDA mode so we get syntax highlighting in .cu files
 (use-package cuda-mode
--- lisp/lp-lsp.el
+++ lisp/lp-lsp.el
@@ -6,11 +6,70 @@
 (use-package lsp-mode
   :diminish lsp-mode
   :straight t
-  :hook ((python-mode-hook c-common-mode-hook julia-mode-hook) . lsp))
+  :hook ((python-mode-hook cuda-mode-hook c-common-mode-hook julia-mode-hook) . lsp)
+  :bind
+  (:map
+   lsp-mode-map
+   ("C-c y n" . lsp-rename)
+   ("C-c y o" . lsp-restart-workspace)
+   ("C-c y c" . lsp-disconnect)
+   ("C-c f" . lsp-format-region))
+  :config
+  (setq lsp-enable-snippet t)
+  (setq lsp-enable-indentation nil)
+  (add-to-list 'lsp-file-watch-ignored "build")
+
+  (add-hook 'lsp-after-open-hook 'lsp-enable-imenu)
+
+  (setq lsp-prefer-capf t)
+
+  ;; Increase the amount of data which Emacs reads from the process. The emacs
+  ;; default is too low 4k considering that the some of the language server
+  ;; responses are in 800k - 3M range. Set to 1MB
+  (setq read-process-output-max (* 1024 1024))
+
+  ;; Use flycheck instead of flymake
+  (setq lsp-prefer-flymake nil)
+
+  )
+
 
 (use-package lsp-ui
   :straight t
-  :diminish lsp-ui-mode)
+  :after lsp-mode
+  :hook (lsp-mode-hook . lsp-ui-mode)
+  :diminish lsp-ui-mode
+  :bind
+  (:map
+   lsp-ui-mode-map
+   ("M-." . lsp-ui-peek-find-definitions)
+   ("M-?" . lsp-ui-peek-find-references))
+  :config
+  (setq
+   ;; Disable sideline hints
+   lsp-ui-imenu-enable nil
+   lsp-ui-sideline-enable nil
+   lsp-ui-sideline-ignore-duplicate t
+   lsp-doc-use-childframe nil
+   ;; Disable imenu
+   lsp-ui-imenu-enable nil
+   ;; Disable ui-doc (already present in minibuffer)
+   lsp-ui-doc-enable nil
+   lsp-ui-doc-header nil
+   lsp-ui-doc-include-signature nil
+   ;; lsp-ui-doc-background (doom-color 'base4)
+   ;; lsp-ui-doc-border (doom-color 'fg)
+   ;; Enable ui-peek
+   lsp-ui-peek-enable t
+                                        ;lsp-ui-peek-fontify t
+   lsp-ui-flycheck-live-reporting t
+   lsp-ui-peek-always-show nil
+   lsp-ui-peek-force-fontify nil
+   lsp-ui-flycheck-enable nil
+   lsp-ui-peek-expand-function (lambda (xs) (mapcar #'car xs)))
+  ;; Flycheck
+  (setq-default flycheck-disabled-checkers '(c/c++-clang
+                                             c/c++-cppcheck c/c++-gcc)) )
 
 (use-package lsp-treemacs
   :straight t
