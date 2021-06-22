05e725e8c37d33c0209a99d17a1106b68677f802
Liam Packer <liampacker@gmail.com>
2021-06-22 09:32:35 -0400

    add next checker after flake8 to be mypy

--- lisp/lp-py.el
+++ lisp/lp-py.el
@@ -2,28 +2,28 @@
 
 (use-package python
   :straight t
-  :defer t
   :mode ("\\.py\\'" . python-mode)
   :interpreter ("python" . python-mode)
   :config
   (setq python-indent-offset 4)
-  (setq python-shell-interpreter "jupyter"
-        python-shell-interpreter-args "console --simple-prompt"
+  (setq python-shell-interpreter "ipython"
+        ;; python-shell-interpreter-args "console --simple-prompt"
         python-shell-prompt-detect-failure-warning nil)
-  (add-to-list 'python-shell-completion-native-disabled-interpreters
-               "jupyter")
+  ;; (add-to-list 'python-shell-completion-native-disabled-interpreters
+  ;;              "jupyter")
   (custom-set-variables
    '(flycheck-python-flake8-executable "python3")
    '(flycheck-python-pycompile-executable "python3")
    '(flycheck-python-pylint-executable "python3"))
+  (flycheck-add-next-checker 'python-flake8 'python-mypy t)
   )
 
 (use-package pyvenv
   :straight t)
 
-(use-package yapfify
-  :straight t
-  :hook (python-mode-hook . yapf-mode))
+;; (use-package yapfify
+;;   :straight t
+;;   :hook (python-mode-hook . yapf-mode))
 
 (use-package julia-mode
   :straight t)
