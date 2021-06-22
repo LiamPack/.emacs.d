a1b277a1c2257b0bae5cb1e3f016f1634e9eb12a
Liam Packer <liampacker@gmail.com>
2021-06-22 09:32:10 -0400

    add mypy flychecker rule to lsp hierarchy

--- lisp/lp-flycheck.el
+++ lisp/lp-flycheck.el
@@ -13,6 +13,18 @@
   ;; https://github.com/abingham/emacs-ycmd
   (when (not (display-graphic-p))
     (setq flycheck-indication-mode nil))
+  (defvar-local my/flycheck-local-cache nil)
+
+  (defun my/flycheck-checker-get (fn checker property)
+    (or (alist-get property (alist-get checker my/flycheck-local-cache))
+        (funcall fn checker property)))
+
+  (advice-add 'flycheck-checker-get :around 'my/flycheck-checker-get)
+
+  (add-hook 'lsp-managed-mode-hook
+            (lambda ()
+              (when (derived-mode-p 'python-mode)
+                (setq my/flycheck-local-cache '((lsp . ((next-checkers . (python-mypy)))))))))
   )
 
 (provide 'lp-flycheck)
