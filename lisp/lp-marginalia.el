dc7a1a17c0145a2c6cf4eac80c4f8c42c5d3acd4
Liam Packer <liampacker@gmail.com>
2021-07-09 14:58:28 -0400

    remove marginalia annotations, remove icomplete, vertico new best friend

--- lisp/lp-marginalia.el
+++ lisp/lp-marginalia.el
@@ -1,8 +1,8 @@
 (use-package marginalia
   :straight (:host github :repo "minad/marginalia" :branch "main")
   :demand
-  :config
-  (setq marginalia-annotators
-        '(marginalia-annotators-heavy
-          marginalia-annotators-light))
+  ;; :config
+  ;; (setq marginalia-annotators
+  ;;       '(marginalia-annotators-heavy
+  ;;         marginalia-annotators-light))
   (marginalia-mode 1))
