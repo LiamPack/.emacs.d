45f18ea1e69fb93833ce0df7eb0901ca380c93b6
Liam Packer <liampacker@gmail.com>
2021-07-20 17:10:58 -0400

    add org-roam-server

--- lisp/lp-org.el
+++ lisp/lp-org.el
@@ -255,6 +255,20 @@ Return nil if no clock is running."
   :config
   (require 'org-protocol))
 
+(use-package org-roam-server
+  :straight t
+  :config
+  (setq org-roam-server-host "localhost"
+        org-roam-server-port 8080
+        org-roam-server-authenticate nil
+        org-roam-server-export-inline-images t
+        org-roam-server-serve-files nil
+        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+        org-roam-server-network-poll t
+        org-roam-server-network-arrows nil
+        org-roam-server-network-label-truncate t
+        org-roam-server-network-label-truncate-length 60
+        org-roam-server-network-label-wrap-length 20))
 
 (use-package nroam
   :straight (:type git :host github
