bc3e9dbc49070103f6a7d6457e7d5f026488732c
Liam Packer <liampacker@gmail.com>
2021-12-31 12:53:52 -0500

    add scratch-pad lisp file, start journal utilities

---
+++ lisp/lp-scratch.el
@@ -0,0 +1,16 @@
+(require 'calendar)
+
+(defvar journal-dir (file-truename "~/org/journal/"))
+
+(defun lp--visit-today-journal-file ()
+  (interactive)
+  (let* ((date-string-list (mapcar #'int-to-string (calendar-current-date)))
+         (year (nth 2 date-string-list))
+         (day (nth 1 date-string-list))
+         (month (nth 0 date-string-list))
+         (date-string (concat  year "-" month "-" day)))
+    (find-file
+     (concat journal-dir date-string ".org"))
+    (insert (format "#+TITLE: %s\n" date-string))))
+
+
