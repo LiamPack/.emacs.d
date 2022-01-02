de92bfd6bea0d09c46083bcfadeec57c2aba6b81
Liam Packer <liampacker@gmail.com>
2022-01-01 19:38:37 -0500

    feat: move to USLS (manual install), load MCT after all other minibuffer-related packages

--- lisp/lp-scratch.el
+++ lisp/lp-scratch.el
@@ -2,15 +2,22 @@
 
 (defvar journal-dir (file-truename "~/org/journal/"))
 
+(defun lp--int-to-string (i)
+  (if (< i 10)
+      (concat "0" (int-to-string i))
+    (int-to-string i)))
+
 (defun lp--visit-today-journal-file ()
   (interactive)
-  (let* ((date-string-list (mapcar #'int-to-string (calendar-current-date)))
+  (let* ((date-string-list (mapcar #'lp--int-to-string (calendar-current-date)))
          (year (nth 2 date-string-list))
          (day (nth 1 date-string-list))
          (month (nth 0 date-string-list))
-         (date-string (concat year "-" month "-" day)))
-    (find-file
-     (concat journal-dir date-string ".org"))
-    (insert (format "#+TITLE: %s\n" date-string))))
-
-
+         (date-string (concat year "-" month "-" day))
+         (filename  (concat journal-dir date-string ".org"))
+         (header (if (file-exists-p filename)
+                     ""
+                   (format "#+title: %s\n" date-string))))
+    (find-file filename)
+    (insert header)
+    (end-of-buffer)))
